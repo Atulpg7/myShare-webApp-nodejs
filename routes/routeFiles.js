35bcdbff076f0178dc42d59a095677c5eda91cc4
atulpg7@gmail.com <atul.kumar@krayonnz.com>
2021-05-13 12:08:57 +0530

    Fixed some bugs

--- routes/routeFiles.js
+++ routes/routeFiles.js
@@ -21,37 +21,40 @@ let upload = multer({
 }).single('myFile');// This should be same as sending from frontend
 
 router.post('/',(req,res)=>{
+        try{
+                //Store file
+                upload(req,res, async (err)=>{ //If we want to use await then async should added
 
-    //Store file
-    upload(req,res, async (err)=>{ //If we want to use await then async should added
+                    //Validting request
+                    if(!req.file){
+                        return res.json({statusCode: 400, msg:"File required!"});
+                    }
 
-        //Validting request
-        if(!req.file){
-            return res.json({statusCode: 400, msg:"File required!"});
+                    //Sending error if there is something wrong while saving the file
+                    if(err){
+                        return res.status(500).send({statusCode: 500, msg:err.message});
+                    }
+
+                    //Store file into database
+                    const file = new File({
+                        filename: req.file.filename,
+                        uuid: uuid4(),
+                        path:req.file.path,
+                        size:req.file.size
+                    });
+
+                    //Respose download link
+                    const response = await file.save();
+                    return res.send( {
+                        status:"success",
+                        statusCode:"200",
+                        message:"file_saved",
+                        file:`${process.env.APP_BASE_URL}/files/${response.uuid}` // Eg:-> http://localhost:3000/files/snsabhjbadjhbzxradrtwqyeu
+                    })
+                });
+        }catch(err){
+            return res.send({statusCode:400, status:"not_success", message:err})
         }
-
-        //Sending error if there is something wrong while saving the file
-        if(err){
-            return res.status(500).send({statusCode: 500, msg:err.message});
-        }
-
-        //Store file into database
-        const file = new File({
-            filename: req.file.filename,
-            uuid: uuid4(),
-            path:req.file.path,
-            size:req.file.size
-        });
-
-        //Respose download link
-        const response = await file.save();
-        return res.send( {
-            status:"success",
-            statusCode:"200",
-            message:"file_saved",
-            file:`${process.env.APP_BASE_URL}/files/${response.uuid}` // Eg:-> http://localhost:3000/files/snsabhjbadjhbzxradrtwqyeu
-        })
-    });
 });
 
 router.post('/send', async (req, res)=>{
