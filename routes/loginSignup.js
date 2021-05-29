9a6dc20bb693d13d376769b7f4d3819eaebac8d3
atulpg7@gmail.com <atul.kumar@krayonnz.com>
2021-05-29 20:32:59 +0530

    Fixed bug in login flow

--- routes/loginSignup.js
+++ routes/loginSignup.js
@@ -2,14 +2,18 @@ const router = require('express').Router();
 const User = require('../models/user');
 
 router.get('/login',async (req,res)=>{
-    const {email, password} = req.body;
+    try{
+        const {email, password} = req.body;
 
-    const user = await User.findOne({email,password});
-    if(!user){
-        res.send({"status":"not success","message":"no account exists"});    
+        const user = await User.findOne({email,password});
+        if(!user){
+            res.send({"status":"not success","message":"no account exists"});    
+        }
+
+        res.send({"status":"success","email":user.email,"message":"login successfull"});
+    }catch(err){
+        return res.send({statusCode:400, status:"not_success",message:err})
     }
-
-    res.send({"status":"success","email":user.email,"message":"login successfull"});
 })
 
 router.post('/signup',async (req,res)=>{
